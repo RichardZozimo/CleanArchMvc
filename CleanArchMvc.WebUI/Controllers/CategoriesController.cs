dc4999e7083273b2ed0ff40d6212895d4f7c1079
Fulano de Tal <richardzozimo@gmail.com>
2023-10-15 19:06:43 -0300

    creating View Models and Controllers for the Create, Edit, Delete and Details functions on our Category page

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -13,11 +15,98 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             _categoryService = categoryService;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var categories = await _categoryService.GetCategories();
 
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO category)
+        {
+            if(ModelState.IsValid)
+            {
+                await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) 
+                return NotFound();
+
+            var categoryVM = await _categoryService.GetById(id);
+
+            if(categoryVM == null) 
+                return NotFound();
+
+            return View(categoryVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) 
+                return NotFound();
+
+            var categoryVM = await _categoryService.GetById(id);
+
+            if (categoryVM == null) 
+                return NotFound();
+
+            return View(categoryVM);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+                return NotFound();
+
+            var categoryVM = await _categoryService.GetById(id);
+
+            if(categoryVM == null)
+                return NotFound();
+
+            return View(categoryVM);
         }
     }
 }
